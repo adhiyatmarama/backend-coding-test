04da827ac46b56c6ca85217c4f52d7c9abd31ec9
adhiyatmarama <adhiyatma.pramayoga@gmail.com>
2020-06-13 20:55:22 +0700

    successfully config winston for logging

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@
 const express = require('express');
 const app = express();
 
+const Logger = require('../services/logger_service');
+const logger = new Logger('app');
+
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
 module.exports = (db) => {
     app.get('/health', (req, res) => {
+        logger.info('request recevied at /health');
         res.send('Healthy');
     });
 
